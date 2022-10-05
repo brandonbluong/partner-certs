498653a14850ca1381b5be95050a29f53eb708a1
Brandon Luong <brandonbluong@gmail.com>
2022-10-04 23:44:25 -0700

    feat: merge (left join) total certs with clean users list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 raw_users_df = pd.read_csv("Personal/raw_users.csv")
+
 drop_columns = [
     "Account | Partner Type",
     "Job Role",
@@ -22,39 +23,51 @@ clean_users_df = (
     .rename(rename_columns, axis=1)
     .drop_duplicates(duplicate_key)
 )
-
-
 raw_sales_certs_df = pd.read_csv("Personal/raw_sales_certs.csv")
-clean_sales_certs_df = raw_sales_certs_df.assign(**{"Certification Type": "Sales"})
+clean_sales_certs_df = raw_sales_certs_df.rename(
+    {"Passed": "Passed Date"}, axis=1
+).assign(**{"Certification Type": "Sales"})
+
 
 raw_technical_certs_df = pd.read_csv("Personal/raw_technical_certs.csv")
-clean_technical_certs_df = raw_technical_certs_df.assign(
-    **{"Certification Type": "Technical"}
-)
+clean_technical_certs_df = raw_technical_certs_df.rename(
+    {"Passed": "Passed Date"}, axis=1
+).assign(**{"Certification Type": "Technical"})
 
 # Combine sales and tech clean dfs
 total_certs_df = pd.concat([clean_sales_certs_df, clean_technical_certs_df])
-# print(total_certs_df)
 
 left_join_keys = ["User", "Account"]
 right_join_keys = ["Full Name", "Company"]
 
-# print(total_certs_df.columns)
-# print(clean_users_df.columns)
+# # print(total_certs_df.columns)
+# # print(clean_users_df.columns)
 
 merged_certs_df = total_certs_df.merge(
-    clean_users_df,
-    how="inner",
-    left_on=left_join_keys,
-    right_on=right_join_keys,
-    validate="1:1",
+    clean_users_df, how="left", left_on=left_join_keys, right_on=right_join_keys
 )
-print(merged_certs_df.columns)
+
+merged_certs_df = merged_certs_df[
+    [
+        "Full Name",
+        "Email",
+        "Title",
+        "Company",
+        "CAM Manager",
+        "Certification Type",
+        "Passed Date",
+        "City",
+        "US State",
+        "Country",
+    ]
+]
+
 print(total_certs_df.shape, merged_certs_df.shape)
+print(merged_certs_df.columns)
 
-# merged_certs = total_certs_df.merge(clean_users_df,
-#     on=)
-# print(merged_certs)
+# # merged_certs = total_certs_df.merge(clean_users_df,
+# #     on=)
+# # print(merged_certs)
 
-# if __name__ == "__main__":
-#     pass
+# # if __name__ == "__main__":
+# #     pass

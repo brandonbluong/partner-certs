49235dcac95931141c3f1bc6eaaa0127842601f5
Brandon Luong <brandonbluong@gmail.com>
2022-10-04 20:24:35 -0700

    feat: drop unused columns from raw users

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    pass
+import pandas as pd
+
+raw_users_df = pd.read_csv("Personal/raw_users.csv")
+drop_columns = [
+    "Account | Partner Type",
+    "Job Role",
+    "Administrative Privileges",
+    "Last Login - Timestamp",
+]
+
+clean_users_df = raw_users_df.drop(drop_columns, axis=1)
+print(clean_users_df.columns)
+print(clean_users_df.head())
+
+# if __name__ == "__main__":
+#     pass
